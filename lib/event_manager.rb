20eea5d051f14b2efe6fb9cb5688b5d59fa627c1
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-07 19:18:14 -0300

    Cleaning zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -24,5 +24,16 @@ contents = CSV.open('../event_attendees.csv',
 contents.each do |row|
   name = row[:first_name]
 	zipcode = row[:zipcode]
+
+	if zipcode.nil?
+		zipcode = "00000"
+	elsif zipcode.length > 5 
+		zipcode = zipcode[0..4]
+	elsif zipcode.length < 5
+		until zipcode.length == 5
+			zipcode = "0" << zipcode
+		end
+	end
+
   puts "#{name} #{zipcode}"
 end
