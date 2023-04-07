bf2008f940be22e6c17663d1ac194337960c2cdd
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-07 16:19:17 -0300

    Add zipcode. Fix 'contents' path

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -16,12 +16,13 @@ end
 require 'csv'
 puts 'EventManager initialized.'
 
-contents = CSV.open('event_attendees.csv',
+contents = CSV.open('../event_attendees.csv',
 	headers: true,
 	header_converters: :symbol
 )
 
 contents.each do |row|
   name = row[:first_name]
-  puts name
+	zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
